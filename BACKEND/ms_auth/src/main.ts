f04d2641ace2a600cc5b8b16e87895d3ce9abe50
Wilber Cutire <wil12fred@MacBook-Pro.local>
2023-12-12 16:40:13 -0500

    feat: [Wilber] adding swagger api doc route

--- BACKEND/ms_auth/src/main.ts
+++ BACKEND/ms_auth/src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  const port = process.env.PORT || 3000;
+  const config = new DocumentBuilder()
+    .setTitle('Microservice Authentication')
+    .setDescription('The authentication API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        description: `Please enter token in following format: Bearer <JWT>`,
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header',
+      },
+      'access-token',
+    )
+    .addTag('Server paths')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/auth/docs', app, document, {
+    explorer: true,
+    swaggerOptions: {
+      filter: true,
+      showRequestDuration: true,
+    },
+  });
+
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
