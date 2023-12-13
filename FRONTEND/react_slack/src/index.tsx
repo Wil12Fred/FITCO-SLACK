e92a54bf7f41657f19f2a00912e6134ee923c0df
Wilber Cutire <wil12fred@MacBook-Pro.local>
2023-12-12 23:25:12 -0500

    fix: [Wilber] react started version

--- FRONTEND/react_slack/src/index.tsx
+++ FRONTEND/react_slack/src/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./slices";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const app = (
+  <React.Fragment>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.Fragment>
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+ReactDOM.render(
+  app,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ FRONTEND/react_slack/src/slices/index.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+});
+
+export const store = configureStore({ reducer: rootReducer, devTools: true });
